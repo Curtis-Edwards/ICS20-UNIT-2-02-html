f0176c01c2b535d9ae27724fa9fc7737863be23b
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-11 00:51:34 +0000

    added linters folder

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@
  */
 function calculateClicked() {
 
-  document.getElementById('area').innerHTML = '<p>12 × 7 =  ' + (12 * 7) + '</p>'
-  document.getElementById('area').innerHTML = '<p>2(12 + 7) =  ' + (2 * (12 + 7)) + '</p>'
+  document.getElementById('area').innerHTML = '<p>12 × 7 = ' + (12 * 7) + '</p>'
+  document.getElementById('perimeter').innerHTML = '<p>2(12 + 7) = ' + (2 * (12 + 7)) + '</p>'
 }
