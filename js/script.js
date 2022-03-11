d75fc247926b31b6a079d1ecc08bc1c0dc527f9a
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-11 00:41:41 +0000

    added math

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Do basic math.
+ */
+function calculateClicked() {
+
+  document.getElementById('area').innerHTML = '<p>12 × 7 =  ' + (12 * 7) + '</p>'
+  document.getElementById('area').innerHTML = '<p>2(12 + 7) =  ' + (2 * (12 + 7)) + '</p>'
+}
